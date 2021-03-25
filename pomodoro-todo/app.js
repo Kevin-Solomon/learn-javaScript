bba81661e47adf59d4e9d3903497da9b41257e74
Kevin-Solomon <kevinsolomon404@gmail.com>
2021-03-26 01:12:04 +0530

    feat - todo list

--- pomodoro-todo/app.js
+++ pomodoro-todo/app.js
@@ -2,8 +2,12 @@
 const startBtn = document.getElementById("start-btn");
 const stopBtn = document.getElementById("stop-btn");
 const timerEl = document.querySelector(".timer");
-console.log(startBtn)
+const plusBtn = document.querySelector(".plus-button")
+const inputText = document.querySelector("#user-input")
+const ulEl = document.querySelector(".list")
 startBtn.addEventListener('click', timer)
+plusBtn.addEventListener('click', addTodo)
+ulEl.contenteditable=true;
 
 let currentTime = (25*60)-1;
 function timer(){
@@ -22,9 +26,16 @@ function timer(){
     console.log(seconds)
     seconds=seconds<10?`0${seconds}`:seconds;
     minutesLeft=minutesLeft<10?`0${minutesLeft}`:minutesLeft;
-    timerEl.innerHTML=` ${minutesLeft}  :  ${seconds}`
+    timerEl.innerHTML=` ${minutesLeft}  :  ${seconds}`;
     currentTime--;
     // console.log(stop)
   },1000)
   
 }  
+function addTodo(event){
+  console.log(inputText.value);
+  let newLi = document.createElement("li");
+  newLi.innerHTML = `${inputText.value} <i class="far fa-trash-alt"></i> <i class="far fa-edit"></i>`;
+  ulEl.appendChild(newLi)
+  inputText.value=""
+}
