d2f31cd4f73f83a3a1f81e2f6d8474b04d2bf9c3
Kevin-Solomon <kevinsolomon404@gmail.com>
2021-03-26 22:09:52 +0530

    feat - added delete and edit options

--- pomodoro-todo/app.js
+++ pomodoro-todo/app.js
@@ -2,14 +2,37 @@
 const startBtn = document.getElementById("start-btn");
 const stopBtn = document.getElementById("stop-btn");
 const timerEl = document.querySelector(".timer");
+const todoItemsEl = document.querySelector(".todo-items");
 const plusBtn = document.querySelector(".plus-button")
-const inputText = document.querySelector("#user-input")
+let inputText = document.querySelector("#user-input")
 const ulEl = document.querySelector(".list")
 startBtn.addEventListener('click', timer)
 plusBtn.addEventListener('click', addTodo)
+todoItemsEl.addEventListener('click', sike)
 ulEl.contenteditable=true;
 
 let currentTime = (25*60)-1;
+function sike(e){
+  console.log(e.target.className)
+  if (e.target.className === "far fa-trash-alt"){
+    console.log(e.target.parentElement.remove())
+  }
+  if (e.target.className === "far fa-check-circle"){
+    console.log(e.target.parentElement.classList.add("completed"))
+    // console.log(e.target.parentElement)
+  }
+  
+  if (e.target.className === "far fa-edit"){
+    console.log(e.target.parentElement.innerText)
+    inputText.value = e.target.parentElement.innerText;
+    e.target.parentElement.remove();
+    // e.target.parentElement.innerText = inputText.value  ;
+    
+    // console.log(e.target.parentElement)
+  }
+
+}
+
 function timer(){
   // let endTime = new Date().getTime() + 25*60*1000;
   // console.log(stop)
@@ -35,7 +58,7 @@ function timer(){
 function addTodo(event){
   console.log(inputText.value);
   let newLi = document.createElement("li");
-  newLi.innerHTML = `${inputText.value} <i class="far fa-trash-alt"></i> <i class="far fa-edit"></i>`;
+  newLi.innerHTML = `${inputText.value} <i class="far fa-check-circle"></i><i class="far fa-trash-alt"></i> <i class="far fa-edit"></i>`;
   ulEl.appendChild(newLi)
   inputText.value=""
 }
